ac0fc18d48a219df2f9490c3be3cb59af896eb36
Evelina Olsson <evol2401@student.miun.se>
2025-10-02 15:24:23 +0200

    Added basics in main.ts with TodoList and DOM elements

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import './style.css'
+import { Todolist } from "./todo";
 
+//Creates a new instance of the TodoList class
+const todoList = new Todolist();
 
-
-
-
-
-
+//Get references for the form and input fields in the DOM
+const form = document.getElementById("todo-form") as HTMLFormElement;
+const taskInput = document.getElementById("task") as HTMLInputElement;
+const priorityInput = document.getElementById("priority") as HTMLInputElement;
+const list = document.getElementById("todo-list") as HTMLUListElement;
